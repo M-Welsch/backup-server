4c1b9801dfc1d939a275ea1bf82356f65f3cbd12
Maximilian Welsch <maxiwelsch@aol.com>
2024-01-16 10:32:31 +0100

    [HIL] add real start/stop scripts

--- hil/stop_bcu_with_test_config.sh
+++ hil/stop_bcu_with_test_config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "test123" | sudo -S systemctl stop backupserver@\\x2d\\x2dconfig\\x20config_hiltest.yaml
+cat /home/base/backup-server/hil/.base_password | sudo -S systemctl stop backupserver@\\x2d\\x2dconfig\\x20config_hiltest.yaml
+

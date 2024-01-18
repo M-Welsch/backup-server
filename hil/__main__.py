8459af28f184d3e0ba485b8c423e112ae6d957b7
Maximilian Staab <MaximilianStaab@gmx.net>
2024-01-18 22:53:57 +0100

    Refactoring

--- hil/__main__.py
+++ hil/__main__.py
@@ -1,5 +1,28 @@
+from pathlib import Path
+
 from hil_execution import execute
 
 
+PCU_SERIAL_DEVICE = '/dev/ttySTLINK'
+PCU_SERIAL_BAUDRATE = 38400
+
+BASE_HOSTNAME_IN_SSH_CONFIG = "basetest"
+
+TESTMODE_FILE_PATH = Path('/home/base/backup-server/software/bcu/config_hiltest.yaml')
+TESTMODE_FILE_CONTENT = """comment: "testmode file"
+
+logger:
+  filename: "temp_logfile_hiltest.log"
+
+backup:
+  rsync_source: "backup_testdata_source"
+
+process:
+  seconds_between_backup_end_and_shutdown: 20
+"""
+
+
 if __name__ == "__main__":
-    execute()
+    execute(
+        PCU_SERIAL_DEVICE, PCU_SERIAL_BAUDRATE, BASE_HOSTNAME_IN_SSH_CONFIG, TESTMODE_FILE_PATH, TESTMODE_FILE_CONTENT
+    )
--- hil/hil_execution.py
+++ hil/hil_execution.py
@@ -1,7 +1,6 @@
 import subprocess
 from datetime import datetime
 from pathlib import Path
-from shutil import copy
 from time import sleep
 from typing import Optional
 
@@ -9,25 +8,8 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
-PCU_SERIAL_DEVICE = '/dev/ttySTLINK'
-PCU_SERIAL_BAUDRATE = 38400
 
-TESTMODE_FILE_CONTENT = """comment: "testmode file"
-
-logger:
-  filename: "temp_logfile_hiltest.log"
-
-backup:
-  rsync_source: "backup_testdata_source"
-  
-process:
-  seconds_between_backup_end_and_shutdown: 20
-"""
-BASE_HOSTNAME_IN_SSH_CONFIG = "basetest"
-
-HIL_RESULT_DIR = Path.cwd().parent/"results"
-
-def setup_logger(result_dir: Path) -> None:
+def setup_logger(log_dir: Path) -> None:
     LOG.debug("Setting up logger...")
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
@@ -38,7 +20,7 @@ def setup_logger(result_dir: Path) -> None:
     console_handler.setLevel(logging.DEBUG)
     console_handler.setFormatter(formatter)
 
-    file_handler = logging.FileHandler(result_dir/'hil.log')
+    file_handler = logging.FileHandler(log_dir / 'hil.log')
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
 
@@ -46,44 +28,49 @@ def setup_logger(result_dir: Path) -> None:
     logger.addHandler(file_handler)
 
 
-def check_prequisites():
-    if not Path(PCU_SERIAL_DEVICE).exists():
-        raise RuntimeError(f"PCU_SERIAL_DEVICE {PCU_SERIAL_DEVICE} is not available")
+def check_prerequisites(device):
+    if not Path(device).exists():
+        raise RuntimeError(f"PCU_SERIAL_DEVICE {device} is not available")
 
-
-def open_terminal(device: str, baudrate: int, logfile: Path) -> subprocess.Popen:
-    subprocess.call(["stty", "-F", device, str(baudrate)])
-    return subprocess.Popen(f"cat {PCU_SERIAL_DEVICE} > {logfile.as_posix()}", shell=True)
 
 class PcuSerialLogger:
-
-    def __init__(self, logfile: Path):
+    def __init__(self, device: str, baud_rate: int, logfile: Path):
         self._logger_process: Optional[subprocess.Popen] = None
+        self._device = device
+        self._baud_rate = baud_rate
         self._logfile = logfile
 
     def __enter__(self):
-        self._logger_process = open_terminal(PCU_SERIAL_DEVICE, PCU_SERIAL_BAUDRATE, self._logfile)
-        LOG.info(f"opened terminal for PCU output with node {PCU_SERIAL_DEVICE}")
+        self._logger_process = self._open_terminal()
+        LOG.info(f"opened terminal for PCU output with node {self._device}")
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._logger_process.kill()
         self._logger_process.wait(timeout=1)
+
+    def _open_terminal(self) -> subprocess.Popen:
+        subprocess.call(["stty", "-F", self._device, str(self._baud_rate)])
+        return subprocess.Popen(f"cat {self._device} > {self._logfile.as_posix()}", shell=True)
+
     def get_logfile(self) -> list[str]:
         with open(self._logfile, 'r') as logfile:
             return logfile.readlines()
 
-def copy_password_file():
-    command = "scp .base_password basetest:/home/base/backup-server/hil"
+
+def copy_password_file(hostname: str):
+    command = f"scp .base_password {hostname}:/home/base/backup-server/hil"
     subprocess.run(command.split())
 
-def remove_password_file():
-    command = "ssh basetest rm /home/base/backup-server/hil/.base_password"
+
+def remove_password_file(hostname: str):
+    command = f"ssh {hostname} rm /home/base/backup-server/hil/.base_password"
     subprocess.run(command.split())
 
-def stop_bcu_service_normalmode(base_name_in_ssh_config: str = BASE_HOSTNAME_IN_SSH_CONFIG, pw_file: str = '.base_password'):
-    copy_password_file()
-    command = f"ssh {base_name_in_ssh_config} '/home/base/backup-server/hil/stop_bcu_with_normal_config.sh'"
+
+def stop_bcu_service_normal_mode(base_hostname: str):
+    copy_password_file(base_hostname)
+    command = f"ssh {base_hostname} '/home/base/backup-server/hil/stop_bcu_with_normal_config.sh'"
     trials = 0
     maximum_trials = 10
     while trials < maximum_trials:
@@ -95,40 +82,42 @@ def stop_bcu_service_normalmode(base_name_in_ssh_config: str = BASE_HOSTNAME_IN_
         sleep(0.5)
     if trials == maximum_trials:
         LOG.warning(f"couldn't stop bcu service within {maximum_trials} trials, waiting 0.5s in between")
-    remove_password_file()
+    remove_password_file(base_hostname)
 
 
-class BcuTestmode:
-    def __init__(self):
-        self._testmode_configfile_on_bcu = Path('/home/base/backup-server/software/bcu/config_hiltest.yaml')
-        arguments = subprocess.check_output(['systemd-escape', f'" --config {self._testmode_configfile_on_bcu.as_posix()}"']).decode().strip()
-        self._bcu_servicename = "backupserver@" + arguments + ".service"
+class BcuTestMode:
+    def __init__(self, base_hostname: str, config_file_path: Path, config_file_content: str):
+        self._base_hostname = base_hostname
+        self._config_file_content = config_file_content
+        self._config_file_path = config_file_path
+        arguments = subprocess.check_output(
+            ['systemd-escape', f'" --config {self._config_file_path.as_posix()}"']
+        ).decode().strip()
+        self._bcu_service_name = f"backupserver@{arguments}.service"
 
-    def start_bcu_service(self, base_name_in_ssh_config: str = BASE_HOSTNAME_IN_SSH_CONFIG, pw_file: str = '.base_password'):
-        # command = f"cat {pw_file} | ssh -tt {base_name_in_ssh_config} 'sudo systemctl start {self._bcu_servicename}'"
-        command = f"ssh {base_name_in_ssh_config} '/home/base/backup-server/hil/start_bcu_with_test_config.sh'"
-        p = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    def _start_bcu_service(self):
+        # command = f"cat {pw_file} | ssh -tt {self._base_hostname} 'sudo systemctl start {self._bcu_service_name}'"
+        command = f"ssh {self._base_hostname} '/home/base/backup-server/hil/start_bcu_with_test_config.sh'"
+        subprocess.run(command, shell=True, stdout=subprocess.PIPE)
         # Todo: verify service runs and retry if not
 
+    def _stop_bcu_service(self):
+        # command = f"cat {pw_file} | ssh -tt {self._base_hostname} 'sudo systemctl stop {self._bcu_service_name}'"
+        command = f"ssh {self._base_hostname} '/home/base/backup-server/hil/stop_bcu_with_test_config.sh'"
+        subprocess.run(command, shell=True, stdout=subprocess.PIPE)
 
-
-    def stop_bcu_service(self, base_name_in_ssh_config: str = BASE_HOSTNAME_IN_SSH_CONFIG, pw_file: str = '.base_password'):
-        # command = f"cat {pw_file} | ssh -tt {base_name_in_ssh_config} 'sudo systemctl stop {self._bcu_servicename}'"
-        command = f"ssh {base_name_in_ssh_config} '/home/base/backup-server/hil/stop_bcu_with_test_config.sh'"
-        p = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
-
-    def copy_test_config(self):
-        LOG.info(f"create testmode file on backup server named {BASE_HOSTNAME_IN_SSH_CONFIG}")
+    def _copy_test_config(self):
+        LOG.info(f"create testmode file on backup server named {self._base_hostname}")
         testmode_tempfile = Path("config_hiltest.yaml")
         with open(testmode_tempfile, "w") as f:
-            f.write(TESTMODE_FILE_CONTENT)
+            f.write(self._config_file_content)
         subprocess.call(
-            f'scp {testmode_tempfile.as_posix()} {BASE_HOSTNAME_IN_SSH_CONFIG}:{self._testmode_configfile_on_bcu.as_posix()}'.split())
+            f'scp {testmode_tempfile.as_posix()} {self._base_hostname}:{self._config_file_path.as_posix()}'.split())
         testmode_tempfile.unlink()
 
-    def remove_test_config(self):
-        LOG.info(f"removing testmode file on backup server named {BASE_HOSTNAME_IN_SSH_CONFIG}.")
-        p = subprocess.run(f'ssh {BASE_HOSTNAME_IN_SSH_CONFIG} rm {self._testmode_configfile_on_bcu.as_posix()}'.split())
+    def _remove_test_config(self):
+        LOG.info(f"removing testmode file on backup server named {self._base_hostname}.")
+        p = subprocess.run(f'ssh {self._base_hostname} rm {self._config_file_path.as_posix()}'.split())
         if not p.returncode == 0:
             LOG.warning("couldn't remove testmode file on BaSe")
 
@@ -138,15 +127,15 @@ class BcuTestmode:
         - we copy a hidden file that contains the password for the base-user on the base
         - we run a script over ssh which reads the password file and uses it to start the backupserver.service (with arguments)
         """
-        self.copy_test_config()
-        copy_password_file()
-        self.start_bcu_service()
+        self._copy_test_config()
+        copy_password_file(self._base_hostname)
+        self._start_bcu_service()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.remove_test_config()
-        self.stop_bcu_service()
-        remove_password_file()
+        self._remove_test_config()
+        self._stop_bcu_service()
+        remove_password_file(self._base_hostname)
 
 
 def create_test_data_on_nas():
@@ -155,58 +144,72 @@ def create_test_data_on_nas():
     LOG.info("created testdata on nas")
 
 
-def _ping(ip_address):
-    p = subprocess.run(f'ping -c 1 -w 1 {ip_address}'.split(), stdout=subprocess.PIPE)
-    return "1 received" in p.stdout.decode()
+def wait_until_rebooted(hostname: str):
+    def _ping(ip_address):
+        p = subprocess.run(f'ping -c 1 -w 1 {ip_address}'.split(), stdout=subprocess.PIPE)
+        return "1 received" in p.stdout.decode()
+
+    def _resolve_ip() -> str:
+        command = "ssh -GT " + hostname + " | awk '/^hostname / { print $2 }'"
+        nas_ip = subprocess.check_output(command, shell=True)
+        return nas_ip.decode().strip()
+
+    def wait_for_no_ping():
+        ip_address = _resolve_ip()
+        LOG.info(f"pinging host '{hostname}' on {ip_address} until it disappears")
+        while _ping(ip_address):
+            sleep(1)
+        LOG.info(f"{hostname} went to sleep")
+
+    def wait_for_ping():
+        ip_address = _resolve_ip()
+        LOG.info(f"pinging host '{hostname}' on {ip_address} until it appears")
+        while not _ping(ip_address):
+            sleep(1)
+        LOG.info(f"{hostname} woke up")
+
+    wait_for_no_ping()  # bcu shutdown
+    wait_for_ping()  # bcu restart/wakeup
 
 
-def resolve_ip(host_name_in_ssh_config: str) -> str:
-    command = "ssh -GT " + host_name_in_ssh_config + " | awk '/^hostname / { print $2 }'"
-    nas_ip = subprocess.check_output(command, shell=True)
-    return nas_ip.decode().strip()
-
-
-def wait_for_no_ping():
-    LOG.info(f"pinging host '{BASE_HOSTNAME_IN_SSH_CONFIG}' on {resolve_ip(BASE_HOSTNAME_IN_SSH_CONFIG)} until it disappears")
-    while _ping(resolve_ip(BASE_HOSTNAME_IN_SSH_CONFIG)):
-        sleep(1)
-    LOG.info(f"{BASE_HOSTNAME_IN_SSH_CONFIG} went to sleep")
-
-
-def wait_for_ping():
-    LOG.info(f"pinging host '{BASE_HOSTNAME_IN_SSH_CONFIG}' on {resolve_ip(BASE_HOSTNAME_IN_SSH_CONFIG)} until it appears")
-    while not _ping(resolve_ip(BASE_HOSTNAME_IN_SSH_CONFIG)):
-        sleep(1)
-    LOG.info(f"{BASE_HOSTNAME_IN_SSH_CONFIG} woke up")
-
-
-def gather_bcu_logfile(result_dir: Path):
-    subprocess.run(f'scp basetest:/home/base/backup-server/software/bcu/temp_logfile_hiltest.log {result_dir/"bcu.log"}'.split())
+def gather_bcu_logfile(base_hostname: str, log_file: Path):
+    subprocess.run(f'scp {base_hostname}:/home/base/backup-server/software/bcu/temp_logfile_hiltest.log {log_file}'.split())
     LOG.info(f"got bcu logfile")
 
 
-def create_result_directory() -> Path:
+def create_result_directory(parent: Path) -> Path:
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    result_dir = (Path.cwd()/"results"/timestamp)
+    result_dir = parent / "results" / timestamp
     result_dir.mkdir(parents=True, exist_ok=True)
     LOG.info(f"created {result_dir.as_posix()} for test results")
     return result_dir
 
 
-def execute():
+def execute(
+        pcu_serial_device: str,
+        pcu_serial_baud_rate: int,
+        base_hostname: str,
+        testmode_file_path: Path,
+        testmode_file_content: str
+):
     # user has to start bcu hardware but NOT the backupserver.service
     # user has to check out the software they want to test
-    check_prequisites()
-    stop_bcu_service_normalmode()
-    result_dir = create_result_directory()
-    setup_logger(result_dir)
+    check_prerequisites(device=pcu_serial_device)
+    stop_bcu_service_normal_mode(base_hostname=base_hostname)
+    result_dir = create_result_directory(parent=Path.cwd())
+    setup_logger(log_dir=result_dir)
     create_test_data_on_nas()
-    with PcuSerialLogger(result_dir/'pcu.log') as pcu_serial_logger:
-        with BcuTestmode() as bcu_testmode:
-            wait_for_no_ping()  # bcu shutdown
-            wait_for_ping()  # bcu restart/wakeup
-        stop_bcu_service_normalmode()
+    with PcuSerialLogger(
+            device=pcu_serial_device, baud_rate=pcu_serial_baud_rate, logfile=result_dir / "pcu.log"
+    ) as pcu_serial_logger:
+        with BcuTestMode(
+                base_hostname=base_hostname,
+                config_file_path=testmode_file_path,
+                config_file_content=testmode_file_content
+        ):
+            wait_until_rebooted(hostname=base_hostname)
+        stop_bcu_service_normal_mode(base_hostname=base_hostname)
     pcu_serial_logger.get_logfile()
-    gather_bcu_logfile(result_dir)
+    gather_bcu_logfile(base_hostname=base_hostname, log_file=result_dir / "bcu.log")
 
     # assert stuff

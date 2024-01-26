2ca57358e8adb7a632d9cc13ceadccb2bd5868ac
Maxi <78823900+M-Welsch@users.noreply.github.com>
2024-01-26 22:23:00 +0100

    [BCU] add udev rule for real backup hdd, set time to wake up to 12h (#36)
    [BCU] minor changes before longterm test

--- software/bcu/__main__.py
+++ software/bcu/__main__.py
@@ -4,12 +4,16 @@ import subprocess
 from datetime import datetime, timedelta
 import argparse
 from pathlib import Path
-import signal
+from typing import Callable
 
+import config
 import pcu
 from config import load_config
 
 LOG = logging.getLogger(__name__)
+
+
+HDD_SPINDOWN_SECONDS = 20
 
 
 def setup_logger(logger_config: dict) -> None:
@@ -32,17 +36,9 @@ def setup_logger(logger_config: dict) -> None:
     logger.addHandler(file_handler)
 
 
-def handle_termination(signum, frame):
-    LOG.info(f"terminated by user. Initiating graceful switchoff. Signal: {signum}, Frame: {frame}")
-    # stop rsync
-    # disengage
-
-
 async def init(logger_config: dict):
     setup_logger(logger_config)
     await pcu.handshake()
-    signal.signal(signal.SIGINT, handle_termination)
-    signal.signal(signal.SIGTERM, handle_termination)
 
 
 async def engage() -> None:
@@ -56,17 +52,25 @@ async def engage() -> None:
             raise RuntimeError("couldn't dock with two trials")
     LOG.debug("Switching HDD power on...")
     await pcu.cmd.power.hdd.on()
-    await asyncio.sleep(2)  # workaround for #26
+    await asyncio.sleep(10)  # workaround for #26
     LOG.debug("Mounting HDD...")
-    subprocess.call(["mount", "/dev/BACKUPHDD"])
+    trials = 0
+    maximum_trials = 3
+    while trials < maximum_trials:
+        subprocess.call("mount /dev/BACKUPHDD", shell=True)
+        await asyncio.sleep(5)
+        if Path('/media/BackupHDD').is_mount():
+            break
+        LOG.warning("mounting of backupHDD failed, try another time")
+        trials += 1
 
 
-async def handle_output(pipe):
+async def handle_output(pipe, log_func: Callable):
     while True:
         line = await pipe.readline()
         if not line:
             break
-        LOG.debug(f"Live Output: {line.decode().rstrip()}")
+        log_func(f"Live Output: {line.decode().rstrip()}")
 
 
 def resolve_ip(host_name_in_ssh_config: str) -> str:
@@ -84,8 +88,8 @@ async def backup(config: dict):
         "-aH",
         "--stats",
         "--delete",
-        f"{nas_ip}::{source}/*",
-        "/media/BackupHDD/backups/current"
+        f"{nas_ip}::{source}/",
+        "/media/BackupHDD/backups/testdata"
     ]
     LOG.debug(f"Backing up with command {' '.join(backup_command)}")
     process = await asyncio.create_subprocess_exec(
@@ -95,25 +99,25 @@ async def backup(config: dict):
         stderr=subprocess.PIPE
     )
 
-    output_task = asyncio.create_task(handle_output(process.stdout))
+    output_task = asyncio.create_task(handle_output(process.stdout, LOG.debug))
+    output_task_stderr = asyncio.create_task(handle_output(process.stderr, LOG.error))
 
     LOG.info("Starting Backup...")
     await process.wait()
     LOG.info("Backup finished.")
     await output_task
-    LOG.debug("Backup output task finished.")
-
-    stderr = await process.stderr.read()
-    if stderr:
-        LOG.error(f"Fehlerausgabe: {stderr.decode()}")
+    LOG.debug("Backup stdout output task finished.")
+    await output_task_stderr
+    LOG.debug("Backup stderr output task finished.")
 
 
 async def disengage() -> None:
     LOG.info("Disconnecting Backup HDD")
     LOG.debug("Unmounting HDD...")
-    subprocess.call(["umount", "/dev/BACKUPHDD"])
+    subprocess.call("umount /dev/BACKUPHDD", shell=True)
     LOG.debug("Switching HDD power off...")
     await pcu.cmd.power.hdd.off()
+    await asyncio.sleep(HDD_SPINDOWN_SECONDS)
     LOG.debug("Undocking...")
     await pcu.cmd.undock()
 
@@ -128,13 +132,13 @@ async def wait_before_shutdown(cfg):
     await asyncio.sleep(sleep_duration)
 
 
-async def set_wakeup_time() -> None:
+async def set_wakeup_time(sleep_time: timedelta) -> None:
     LOG.info("Programming PCU. Setting current time and time for next wakeup")
     await pcu.set.date.now(datetime.now())
     LOG.debug(f"read current time from pcu: {await pcu.get.date.now()}")
-    await pcu.set.date.wakeup(datetime.now() + timedelta(minutes=1))
+    await pcu.set.date.wakeup(datetime.now() + sleep_time)
     LOG.debug(f"read wakeup time from pcu: {await pcu.get.date.wakeup()}")
-    await pcu.set.date.backup(datetime.now() + timedelta(minutes=1))
+    await pcu.set.date.backup(datetime.now() + sleep_time)
     LOG.debug(f"read backup time from pcu: {await pcu.get.date.backup()}")
 
 
@@ -146,17 +150,17 @@ async def shutdown():
 
 async def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--no-shutdown", default=False, required=False)
+    parser.add_argument("--no-shutdown", default=False, required=False, action='store_true')
     parser.add_argument("--config", default="config.yaml", type=str, required=False)
     args = parser.parse_args()
     cfg = load_config(Path(args.config))
-    LOG.info(f"loading config file {args.config}")
     await init(cfg["logger"])
+    LOG.info(f"loading config file {args.config}")
     await engage()
     await backup(cfg["backup"])
     await disengage()
     await wait_before_shutdown(cfg)
-    await set_wakeup_time()
+    await set_wakeup_time(config.get_sleep_time(cfg["process"]["time_between_backups"]))
     if not args.no_shutdown:
         await shutdown()
 
--- software/bcu/config.py
+++ software/bcu/config.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import timedelta
 from pathlib import Path
 import yaml
 
@@ -13,3 +14,10 @@ def _load_cfg_from_yaml(file: Path) -> dict:
 def load_config(config_file: Path) -> dict:
     config = _load_cfg_from_yaml(config_file)
     return config
+
+
+def get_sleep_time(config) -> timedelta:
+    days = config.get("days", 7)
+    hours = config.get("hours", 0)
+    minutes = config.get("minutes", 0)
+    return timedelta(days=days, hours=hours, minutes=minutes)
